54b816eefa146490847c88fa9bcf50fa3efd27b4
diki <muliaandiki@gmaiil.com>
2025-10-21 16:40:32 +0700

    feat: initial setup for expo native

---
+++ Expo/app/index.tsx
@@ -0,0 +1,12 @@
+import "@/styles/global.css";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-6xl text-red-800">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
